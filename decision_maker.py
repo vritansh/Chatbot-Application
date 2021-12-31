5977117517c83a377dc44f507291ebe6a7d3ed74
vritansh <vritansh14@gmail.com>
2021-12-31 21:40:47 +0530

    Update decision_maker.py
    Generic code

--- decision_maker.py
+++ decision_maker.py
@@ -5,8 +5,13 @@ def decision_analyser(predicted_value):
             
           if predicted_value in ACCEPTED_VALUES:
                if(predicted_value== API_USAGE.NOCALL:
+                  print("Fetch Success")
                   
-    
+               if predicted_value=="Social_Skill" : 
+                  print("Fetch Done") 
+               if predicted_value == "Stock_Preduction" :
+                  print("Stock Found") 
+         
     
     
   
